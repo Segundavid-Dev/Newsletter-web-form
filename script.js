74079ec88e9af460885aa4a9455a45228605df20
Segun David <segdavid03@gmail.com>
2024-11-04 13:00:49 +0100

    feat: Implemented modal click feature

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 "use strict";
 
-console.log("hello world");
+const input_email = document.querySelector("#email");
+const submit = document.querySelector(".submit");
+const body = document.body;
+
+submit.addEventListener("click", function (e) {
+  //   e.preventDefault(); // prevent default behaviour of the loading submit input
+  console.log(input_email.value);
+});
