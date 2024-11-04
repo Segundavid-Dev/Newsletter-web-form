a021f6a0fbacb91d0792b4aed49900a97ed7c6ab
Segun David <segdavid03@gmail.com>
2024-11-04 17:06:05 +0100

    feat: dismiss modal notification from DOM functionality

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 "use strict";
 
+// getting selectors
 const input_email = document.querySelector("#email");
 const submit = document.querySelector(".submit");
-const body = document.body;
+const successMessageModal = document.querySelector(".success-message-modal");
+const formWrapper = document.querySelector(".form-wrapper");
+const form = document.querySelector(".form");
+const container = document.querySelector(".container");
+const emailRecipent = document.querySelector(".email-recipent");
+const dismissModal = document.querySelector(".dismiss-modal");
+// const body = document.body;
 
 submit.addEventListener("click", function (e) {
-  //   e.preventDefault(); // prevent default behaviour of the loading submit input
-  console.log(input_email.value);
+  e.preventDefault();
+  container.classList.add("hidden");
+  successMessageModal.classList.remove("hidden");
+  emailRecipent.textContent = input_email.value;
+});
+
+// remove modal notifaction an
+dismissModal.addEventListener("click", function () {
+  alert("dimiss modal");
 });
